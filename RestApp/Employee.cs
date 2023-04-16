842cf3e56a624c37b0ab088025be71da85dde223
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-16 17:27:19 +0530

    Testing rest apis

---
+++ RestApp/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApp
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
