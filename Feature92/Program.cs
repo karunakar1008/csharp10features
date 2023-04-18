d981208aba09d6534e94a5d9078afa901269b35a
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-19 02:41:23 +0530

    Change message

--- Feature92/Program.cs
+++ Feature92/Program.cs
@@ -3,11 +3,11 @@ using Feature92;
 using System;
 using System.Text;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Assignment and declaration in same deconstruction");
 var rectangle = new Rectangle(10, 20);
 (int h, int w) = rectangle;
 
 int height = 0;
 //int width = 0;
-//(height, int width) = rectangle; //assignment and declaration is not possible in deconstructor in c39
+(height, int width) = rectangle; //assignment and declaration is not possible in deconstructor in c# 10
 
