fb03c286c69b78c46357cb919f03f6863ec2977f
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-19 02:30:55 +0530

    String builder improvements

--- Feature3/Program.cs
+++ Feature3/Program.cs
@@ -1,4 +1,12 @@
 ﻿using Feature3;
+using System.Text;
 
 Console.WriteLine(ApiRoutes.GetByName);
 Console.ReadKey();
+
+var fullName = new StringBuilder("Bhogyari");
+var LastName = "karunakar";
+
+fullName.Append(LastName);
+fullName.Append($"Hello {LastName}, how are you?");
+Console.WriteLine(fullName);
