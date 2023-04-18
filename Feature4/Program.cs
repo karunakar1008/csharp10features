41a3d3aecbb122b5f4e8af757823e351b59e11b4
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-18 20:38:56 +0530

    Lambda improvements

---
+++ Feature4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lambda improvements");
+Console.WriteLine("Type inrefence is not supported in c# 9");
+
+//Func<string> helloworld = () => "hellow world"; //
+
+var helloWorld = () => "Hello Karuna";
+var getId = () => 400;
+
+Console.WriteLine(helloWorld());
+Console.ReadKey();
