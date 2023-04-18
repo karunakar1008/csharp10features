a1a8e21d5c26838de7e3357c66b6c1c4256c0c87
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-19 02:30:33 +0530

    assignment and declaration is not possible in deconstructor

---
+++ Feature92/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Feature92;
+using System;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var rectangle = new Rectangle(10, 20);
+(int h, int w) = rectangle;
+
+int height = 0;
+//int width = 0;
+//(height, int width) = rectangle; //assignment and declaration is not possible in deconstructor in c39
+
---
+++ Feature92/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Feature92
+{
+    internal class Rectangle
+    {
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        internal void Deconstruct(out int height, out int width) //Deconstruct an object
+        {
+            height = Height;
+            width = Width;
+        }
+    }
+}
