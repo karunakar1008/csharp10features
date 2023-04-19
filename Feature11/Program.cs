054adaf6cdfc87f2e4984c5a90c63430fd91833b
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-19 09:03:39 +0530

    string literals in c sharp 7

---
+++ Feature11/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Raw string literals");
+string longMessage = """
+    This is a long message.
+    It has several lines.
+        Some are indented
+                more than others.
+    Some should start at the first column.
+    Some have "quoted text" in them.
+    """;
+Console.WriteLine(longMessage);
+
+var Longitude = 10;
+var Latitude = 30;
+
+var location = $$"""
+   You are at {{Longitude}}, {{Latitude}}
+   """;
+Console.WriteLine(location);
